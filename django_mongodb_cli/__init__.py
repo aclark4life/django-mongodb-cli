23b69c620d49fe5d15cf6084cc76e013348922a2
Jeffrey A. Clark <aclark@aclark.net>
2024-11-19 19:16:51 -0500

    Add installurl

--- django_mongodb_cli/__init__.py
+++ django_mongodb_cli/__init__.py
@@ -171,6 +171,57 @@ def installmiddleware(middleware_name):
 
 
 @click.command()
+@click.argument("url_name")
+def installurl(url_name):
+    """
+    Add url_name to the urlpatterns list in the Django urls file located at urls_path
+    """
+    settings_path = os.path.join("mongo_project", "urls.py")
+    import_statement = "from django.urls import include\n"
+
+    try:
+        with open(settings_path, "r") as file:
+            lines = file.readlines()
+
+        installed_urls_started = False
+        modified = False
+
+        for i, line in enumerate(lines):
+            # Check if the import statement is already in the file
+            if import_statement not in lines:
+                # Find the position to insert (typically after other imports)
+                for idx, line in enumerate(lines):
+                    if line.startswith("from") or line.startswith("import"):
+                        continue
+                    lines.insert(idx, import_statement)
+                    break
+            # Detect the urlpatterns list
+            if "urlpatterns" in line and "=" in line:
+                installed_urls_started = True
+
+            # Check if the app is already listed
+            if installed_urls_started and url_name in line:
+                click.echo(f"{url_name} is already installed.")
+                return
+
+            # Add the app at the end of the urlpatterns list
+            if installed_urls_started and "]" in line:  # End of the list
+                lines.insert(i, f'    path("", include("{url_name}")),\n')
+                modified = True
+                break
+
+        if modified:
+            with open(settings_path, "w") as file:
+                file.writelines(lines)
+            click.echo(f"Added {url_name} to urlpatterns in {settings_path}.")
+        else:
+            click.echo(f"Could not find urlpatterns in {settings_path}.")
+
+    except Exception as e:
+        click.echo(f"Error: {e}")
+
+
+@click.command()
 def runserver():
     # Start MongoDB
     mongodb = subprocess.Popen(["mongo-launch", "single"])
@@ -318,6 +369,7 @@ cli.add_command(clone)
 cli.add_command(createsuperuser)
 cli.add_command(installapp)
 cli.add_command(installmiddleware)
+cli.add_command(installurl)
 cli.add_command(runserver)
 cli.add_command(startapp)
 cli.add_command(startproject)
