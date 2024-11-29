7cdee01c7e0861aea25f77c91b8fd4d096274ad0
Jeffrey A. Clark <aclark@aclark.net>
2024-11-29 13:47:59 -0500

    Refactor

--- django_mongodb_cli/startproject.py
+++ django_mongodb_cli/startproject.py
@@ -10,30 +10,25 @@ import subprocess
 @click.option("-dm", "--django-mongodb", is_flag=True, help="Use wagtail template")
 def startproject(delete, wagtail_mongodb, django_mongodb):
     """Run startproject command with the template from src/django-mongodb-project."""
+
     if delete:
-        if os.path.isdir("apps"):
-            shutil.rmtree("apps")
-            print("Removed directory: apps")
-        else:
-            print("Skipping: apps does not exist")
+        items = {
+            "apps": os.path.isdir,
+            "backend": os.path.isdir,
+            "mongo_migrations": os.path.isdir,
+            "manage.py": os.path.isfile,
+        }
 
-        if os.path.isdir("backend"):
-            shutil.rmtree("backend")
-            print("Removed directory: backend")
-        else:
-            print("Skipping: backend does not exist")
-
-        if os.path.isdir("mongo_migrations"):
-            shutil.rmtree("mongo_migrations")
-            print("Removed directory: mongo_migrations")
-        else:
-            print("Skipping: mongo_migrations does not exist")
-
-        if os.path.isfile("manage.py"):
-            os.remove("manage.py")
-            print("Removed file: manage.py")
-        else:
-            print("Skipping: manage.py does not exist")
+        for item, check_function in items.items():
+            if check_function(item):
+                if os.path.isdir(item):
+                    shutil.rmtree(item)
+                    print(f"Removed directory: {item}")
+                elif os.path.isfile(item):
+                    os.remove(item)
+                    print(f"Removed file: {item}")
+            else:
+                print(f"Skipping: {item} does not exist")
 
         exit()
 
