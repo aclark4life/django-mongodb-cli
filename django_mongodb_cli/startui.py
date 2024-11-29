8a9b7c9e55c84ae4296485c712ff04d5ebd1ce04
Jeffrey A. Clark <aclark@aclark.net>
2024-11-29 13:15:47 -0500

    Move startui to startui

--- django_mongodb_cli/startui.py
+++ django_mongodb_cli/startui.py
@@ -1,4 +1,6 @@
 import click
+import os
+import shutil
 import sys
 import subprocess
 
@@ -8,7 +10,6 @@ from .install import install
 from .runtests import runtests
 from .startapp import startapp
 from .startproject import startproject
-from .startui import startui
 
 
 @click.command()
@@ -35,6 +36,39 @@ def runserver(mongo_single):
     subprocess.run([sys.executable, "manage.py", "runserver"])
     if mongo_single:
         mongodb.terminate()
+
+
+@click.command()
+@click.option("-d", "--delete", is_flag=True, help="Delete existing project files")
+def startui(delete):
+    """Run webpack_init command to create frontend directory."""
+    if delete:
+        if os.path.isdir("frontend"):
+            shutil.rmtree("frontend")
+            print("Removed directory: frontend")
+        else:
+            print("Skipping: frontend does not exist")
+
+        for frontend_file in [
+            ".babelrc",
+            ".browserslistrc",
+            ".eslintrc",
+            ".nvmrc",
+            ".stylelintrc.json",
+            "package-lock.json",
+            "package.json",
+            "postcss.config.js",
+        ]:
+            if os.path.isfile(frontend_file):
+                os.remove(frontend_file)
+                print(f"Removed file: {frontend_file}")
+            else:
+                print(f"Skipping: {frontend_file} does not exist")
+        exit()
+
+    click.echo(
+        subprocess.run([sys.executable, "manage.py", "webpack_init", "--no-input"])
+    )
 
 
 @click.group()
