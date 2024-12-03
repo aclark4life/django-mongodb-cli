35f15b49774c8c22d4d92fea422b860b35615329
Jeffrey A. Clark <aclark@aclark.net>
2024-12-03 12:03:51 -0500

    s/parse/parse_uri/

--- mongodb_settings.py
+++ mongodb_settings.py
@@ -17,4 +17,4 @@ PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
 SECRET_KEY = "django_tests_secret_key"
 USE_TZ = False
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb://localhost:27017/djangotests")
-DATABASES["default"] = django_mongodb.parse(DATABASE_URL)
+DATABASES["default"] = django_mongodb.parse_uri(DATABASE_URL)
