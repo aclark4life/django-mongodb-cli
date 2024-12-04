a05b08a339205f4a452d8ede3d6fe2d98405484b
Jeffrey A. Clark <aclark@aclark.net>
2024-12-04 13:59:17 -0500

    Name arg is optional

--- django_mongodb_cli/startapp.py
+++ django_mongodb_cli/startapp.py
@@ -5,21 +5,29 @@ import subprocess
 
 
 @click.command()
-@click.argument("name")
-def startapp(name):
+@click.option("-d", "--delete", is_flag=True, help="Delete existing apps")
+@click.argument("name", required=False)
+def startapp(name, delete):
     """Run startapp command with the template from src/django-mongodb-app."""
-    if os.path.exists("manage.py"):
-        click.echo(
-            subprocess.run(
-                [
-                    sys.executable,
-                    "manage.py",
-                    "startapp",
-                    name,
-                    "--template",
-                    os.path.join(os.path.join("src", "django-mongodb-app")),
-                ]
-            )
+
+    if delete:
+        if os.path.exists("apps"):
+            click.echo(subprocess.run(["rm", "-rf", "apps"]))
+        else:
+            click.echo("No apps directory found.")
+        sys.exit(0)
+
+    if not os.path.exists("apps"):
+        os.makedirs("apps")
+    click.echo(
+        subprocess.run(
+            [
+                "django-admin",
+                "startapp",
+                name,
+                "--template",
+                os.path.join(os.path.join("..", "src", "django-mongodb-app")),
+            ],
+            cwd="apps",
         )
-    else:
-        exit("Please run this command in the root of your Django project.")
+    )
