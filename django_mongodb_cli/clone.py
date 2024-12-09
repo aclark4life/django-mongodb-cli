4ab80938bc3ffe8380a4f6bf1f1a529ce45ba67c
Jeffrey A. Clark <aclark@aclark.net>
2024-12-09 17:27:20 -0500

    refactor

--- django_mongodb_cli/clone.py
+++ django_mongodb_cli/clone.py
@@ -25,7 +25,6 @@ def clone(
     remote,
     fetch,
 ):
-    """Clone `dev` repositories in `tool.django_mongodb_cli` section of `pyproject.toml`."""
     if delete:
         if os.path.isdir("src"):
             shutil.rmtree("src")
@@ -33,30 +32,21 @@ def clone(
         else:
             click.echo("Skipping: src does not exist")
         return
-    # Read and parse the pyproject.toml file
     with open(pyproject_path, "r") as f:
         pyproject_data = toml.load(f)
-
-    # Get the list of repositories
     repos = pyproject_data.get("tool", {}).get("django_mongodb_cli", {}).get("dev", [])
-
     if not repos:
         click.echo("No repositories found under [tool.django_mongodb_cli] dev")
         return
-
-    # Regex to extract the URL from the string
     url_pattern = re.compile(r"git\+ssh://[^@]+@([^@]+)")
     branch_pattern = re.compile(
         r"git\+ssh://git@github\.com/[^/]+/[^@]+@([a-zA-Z0-9_\-\.]+)\b"
     )
     upstream_pattern = re.compile(r"#\s*upstream:\s*([\w-]+)")
-
-    # Clone each repository
     for repo_entry in repos:
         url_match = url_pattern.search(repo_entry)
         branch_match = branch_pattern.search(repo_entry)
         upstream_match = upstream_pattern.search(repo_entry)
-
         if url_match:
             repo_url = url_match.group(0)
             repo_name = os.path.basename(repo_url)
@@ -65,10 +55,7 @@ def clone(
             pyproject_toml = os.path.join(clone_path, "pyproject.toml")
             setup_py = os.path.join(clone_path, "setup.py")
 
-            if update:
-                click.echo(f"Updating {repo_url} in {clone_path} (branch: {branch})")
-                subprocess.run(["git", "pull"], cwd=clone_path)
-
+            # Clone repository
             if not os.path.exists(clone_path):
                 click.echo(f"Cloning {repo_url} into {clone_path} (branch: {branch})")
                 try:
@@ -81,6 +68,7 @@ def clone(
             else:
                 click.echo(f"Skipping {repo_url} in {clone_path} (branch: {branch})")
 
+            # Install package in development mode
             if os.path.exists(pyproject_toml):
                 subprocess.run(
                     [sys.executable, "-m", "pip", "install", "-e", clone_path]
@@ -88,18 +76,22 @@ def clone(
             if os.path.exists(setup_py):
                 subprocess.run([sys.executable, "setup.py", "develop"], cwd=clone_path)
 
+            # Install pre-commit hooks
+            if os.path.isfile(os.path.join(clone_path, ".pre-commit-config.yaml")):
+                subprocess.run(["pre-commit", "install"], cwd=clone_path)
+
+            # Conditionally fetch from remotes, update checkouts, and add upstream remotes
+            if fetch:
+                subprocess.run(["git", "fetch", "upstream"], cwd=clone_path)
+            if update:
+                click.echo(f"Updating {repo_url} in {clone_path} (branch: {branch})")
+                subprocess.run(["git", "pull"], cwd=clone_path)
             if remote and upstream_match:
                 remote = f"https://github.com/{upstream_match.group(1)}/{repo_name}"
                 subprocess.run(
                     ["git", "remote", "add", "upstream", remote], cwd=clone_path
                 )
                 subprocess.run(["git", "remote", "-v", "show"], cwd=clone_path)
-
-            if fetch:
-                subprocess.run(["git", "fetch", "upstream"], cwd=clone_path)
-
-            if os.path.isfile(os.path.join(clone_path, ".pre-commit-config.yaml")):
-                subprocess.run(["pre-commit", "install"], cwd=clone_path)
         else:
             click.echo(f"Invalid repository entry: {repo_entry}")
     click.echo("All repositories cloned successfully.")
