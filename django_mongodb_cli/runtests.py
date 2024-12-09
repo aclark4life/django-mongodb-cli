7307efd8d4e2d249cd6aaf2fa4dbdca2aa956465
Jeffrey A. Clark <aclark@aclark.net>
2024-12-09 14:57:41 -0500

    refactor

--- django_mongodb_cli/runtests.py
+++ django_mongodb_cli/runtests.py
@@ -2,31 +2,24 @@ import click
 import os
 import shutil
 import subprocess
-from .utils import mongo_launch
 
 
 @click.command()
 @click.argument("modules", nargs=-1)
 @click.option("-k", "--keyword", help="Filter tests by keyword")
 @click.option(
-    "-l", "--list-tests", default=False, is_flag=True, help="List available tests"
+    "-l", "--show-tests", default=False, is_flag=True, help="List available tests"
 )
 @click.option(
-    "-m", "--mongo-single", is_flag=True, help="Launch a single MongoDB instance"
+    "-d",
+    "--show-command",
+    is_flag=True,
+    help="Perform a dry run without executing tests",
 )
-@click.option(
-    "--dry-run", is_flag=True, help="Perform a dry run without executing tests"
-)
-def runtests(modules, keyword, list_tests, dry_run, mongo_single):
+def runtests(modules, keyword, show_tests, show_command):
     """
     Run tests for specified modules with an optional keyword filter.
     """
-    if list_tests:
-        click.echo(subprocess.run(["ls", os.path.join("src", "django", "tests")]))
-        click.echo(
-            subprocess.run(["ls", os.path.join("src", "django-mongodb", "tests")])
-        )
-        exit()
 
     shutil.copyfile(
         "mongodb_settings.py",
@@ -40,24 +33,21 @@ def runtests(modules, keyword, list_tests, dry_run, mongo_single):
     command.extend(["--debug-sql"])
     command.extend(["--noinput"])
 
-    # Add modules to the command
     command.extend(modules)
 
-    # Add keyword filter if provided
     if keyword:
         command.extend(["-k", keyword])
 
     click.echo(f"Running command: {' '.join(command)}")
-    if dry_run:
+
+    if show_command:
         exit()
 
-    if mongo_single:
-        # Start MongoDB
-        mongodb = subprocess.Popen(mongo_launch())
+    if show_tests:
+        click.echo(subprocess.run(["ls", os.path.join("src", "django", "tests")]))
+        click.echo(
+            subprocess.run(["ls", os.path.join("src", "django-mongodb", "tests")])
+        )
+        exit()
 
-    # Execute the test command
     subprocess.run(command, stdin=None, stdout=None, stderr=None)
-
-    if mongo_single:
-        # Terminate MongoDB
-        mongodb.terminate()
