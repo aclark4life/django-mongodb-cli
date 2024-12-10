6d32d6245859a042cf72dc4c624b8539af898c0a
Jeffrey A. Clark <aclark@aclark.net>
2024-12-09 20:44:56 -0500

    Update template

--- project_template/project_name/settings/__init__.py
+++ project_template/project_name/settings/__init__.py
@@ -4,6 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DATABASES = {"default": django_mongodb.parse_uri(os.environ.get("MONGODB_URI"))}
 DEBUG = True
+DEFAULT_AUTO_FIELD = "django_mongodb.fields.ObjectIdAutoField"
 INSTALLED_APPS = [
     "{{ project_name }}.mongo_apps.MongoAuthConfig",
     "django.contrib.contenttypes",
