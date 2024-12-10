d3701bc7af9b06bcd02faae18ddbf92fb5268b52
Jeffrey A. Clark <aclark@aclark.net>
2024-12-09 20:18:36 -0500

    Update template

--- project_template/project_name/settings/__init__.py
+++ project_template/project_name/settings/__init__.py
@@ -1,6 +1,7 @@
 import django_mongodb
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DATABASES = {"default": django_mongodb.parse_uri(os.environ.get("MONGODB_URI"))}
 DEBUG = True
 INSTALLED_APPS = [
