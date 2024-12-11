e4f5589a79a7c713e0fb6f34f75957bfe6861a1f
Jeffrey A. Clark <aclark@aclark.net>
2024-12-11 11:45:52 -0500

    Add wagtail

--- django_mongodb_cli/runtests.py
+++ django_mongodb_cli/runtests.py
@@ -27,7 +27,7 @@ def runtests(modules, keyword, wagtail):
         )
         runtests_py = "./runtests.py"
         test_settings = [
-            "settings.py",
+            "wagtail_settings.py",
             os.path.join("src", "wagtail", "wagtail", "test", "mongo_settings.py"),
             "wagtail.test.mongo_settings",
             os.path.join("src", "wagtail"),
@@ -35,7 +35,7 @@ def runtests(modules, keyword, wagtail):
     else:
         runtests_py = os.path.join("src", "django", "tests", "runtests.py")
         test_settings = [
-            "mongo_settings.py",
+            "django_settings.py",
             os.path.join("src", "django", "tests", "mongo_settings.py"),
             "mongo_settings",
             ".",
