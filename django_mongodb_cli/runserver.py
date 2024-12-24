e139806bd48fa1635621b66185cc60ee5ea4f9e2
Jeffrey A. Clark <aclark@aclark.net>
2024-12-24 13:15:09 -0500

    If not manage.py use django-admin

--- django_mongodb_cli/runserver.py
+++ django_mongodb_cli/runserver.py
@@ -11,4 +11,9 @@ def runserver():
     if os.environ.get("MONGODB_URI"):
         click.echo(os.environ["MONGODB_URI"])
 
-    subprocess.run([sys.executable, "manage.py", "runserver"])
+    if os.path.exists("manage.py"):
+        command = [sys.executable, "manage.py"]
+    else:
+        command = ["django-admin"]  # Use a list for consistency
+
+    subprocess.run(command + ["runserver"])
