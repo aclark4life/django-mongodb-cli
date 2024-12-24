46956bc8a879fb721b0cf568d10a8895ad8d3b15
Jeffrey A. Clark <aclark@aclark.net>
2024-12-24 13:10:38 -0500

    Echo MONGODB_URI if found

--- django_mongodb_cli/runserver.py
+++ django_mongodb_cli/runserver.py
@@ -1,4 +1,5 @@
 import click
+import os
 import sys
 import subprocess
 
@@ -6,5 +7,8 @@ import subprocess
 @click.command()
 def runserver():
     """Start MongoDB and run the Django development server."""
+
+    if os.environ.get("MONGODB_URI"):
+        click.echo(os.environ["MONGODB_URI"])
 
     subprocess.run([sys.executable, "manage.py", "runserver"])
