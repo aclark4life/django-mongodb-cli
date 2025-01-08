7b4fa2055dc299458329735d10923dd11349a7c9
Jeffrey A. Clark <aclark@aclark.net>
2025-01-08 14:38:38 -0500

    Update db settings

--- test_settings/settings_django.py
+++ test_settings/settings_django.py
@@ -2,19 +2,11 @@ import django_mongodb_backend
 import os
 
 
+MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb://localhost:27017/djangotests")
 DATABASES = {
-    "default": {
-        "ENGINE": "django_mongodb_backend",
-        "NAME": "djangotests",
-    },
-    "other": {
-        "ENGINE": "django_mongodb_backend",
-        "NAME": "djangotests-other",
-    },
+    "default": django_mongodb_backend.parse_uri(MONGODB_URI),
 }
 DEFAULT_AUTO_FIELD = "django_mongodb_backend.fields.ObjectIdAutoField"
 PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
 SECRET_KEY = "django_tests_secret_key"
 USE_TZ = False
-DATABASE_URL = os.environ.get("MONGODB_URI", "mongodb://localhost:27017/djangotests")
-DATABASES["default"] = django_mongodb_backend.parse_uri(DATABASE_URL)
