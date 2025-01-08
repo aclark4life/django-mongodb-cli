e9a4264641707b1eb4209d0229a02b272a7965a6
Jeffrey A. Clark <aclark@aclark.net>
2025-01-08 17:07:45 -0500

    Refactor

--- django_mongodb_cli/runtests.py
+++ django_mongodb_cli/runtests.py
@@ -1,69 +1,14 @@
 import click
 import os
-import shutil
 import subprocess
 
-
-def copy_mongo_apps(test_dir, app_type):
-    """Copy the appropriate mongo_apps file based on the app type."""
-    app_files = {
-        "wagtail": "apps_wagtail.py",
-        "django_filter": "apps_filter.py",
-        "django_rest_framework": "apps_drf.py",
-    }
-    if app_type in app_files:
-        click.echo(click.style(f"Copying mongo_apps to {app_type}", fg="blue"))
-        shutil.copyfile(
-            os.path.join("test_apps", app_files[app_type]),
-            os.path.join(test_dir, "mongo_apps.py"),
-        )
-
-
-def copy_mongo_migrations(test_dir):
-    """Copy mongo_migrations to the specified test directory."""
-    click.echo(click.style("Copying mongo_migrations", fg="blue"))
-    target_dir = os.path.join(test_dir, "mongo_migrations")
-    if not os.path.exists(target_dir):
-        shutil.copytree(
-            os.path.join("project_templates", "project_template", "mongo_migrations"),
-            target_dir,
-        )
-
-
-def get_test_settings(test_dir, app_type):
-    """Retrieve settings for the specified app type."""
-    settings_map = {
-        "wagtail": [
-            "settings_wagtail.py",
-            "mongo_settings.py",
-            "wagtail.test.mongo_settings",
-            os.path.join("src", "wagtail"),
-        ],
-        "django_filter": [
-            "settings_filter.py",
-            "mongo_settings.py",
-            "tests.mongo_settings",
-            os.path.join("src", "django-filter"),
-        ],
-        "django_rest_framework": [
-            "settings_drf.py",
-            "conftest.py",
-            "tests.conftest",
-            os.path.join("src", "django-rest-framework"),
-        ],
-        "default": [
-            "settings_django.py",
-            "mongo_settings.py",
-            "mongo_settings",
-            ".",
-        ],
-    }
-    return [
-        os.path.join("test_settings", settings_map[app_type][0]),
-        os.path.join(test_dir, settings_map[app_type][1]),
-        settings_map[app_type][2],
-        settings_map[app_type][3],
-    ]
+from .utils import (
+    copy_mongo_migrations,
+    copy_mongo_apps,
+    copy_test_settings,
+    test_dirs_map,
+    runtests_py_map,
+)
 
 
 @click.command()
@@ -96,36 +41,12 @@ def runtests(
         else "default"
     )
 
-    test_dirs_map = {
-        "wagtail": [
-            os.path.join("src", "wagtail", "wagtail", "test"),
-            os.path.join("src", "wagtail", "wagtail", "tests"),
-        ],
-        "django_filter": [os.path.join("src", "django-filter", "tests")],
-        "django_rest_framework": [
-            os.path.join("src", "django-rest-framework", "tests")
-        ],
-        "default": [
-            os.path.join("src", "django", "tests"),
-            os.path.join("src", "django-mongodb-backend", "tests"),
-        ],
-    }
-
     test_dirs = test_dirs_map[app_type]
     main_test_dir = test_dirs[0]
 
     copy_mongo_migrations(main_test_dir)
     copy_mongo_apps(main_test_dir, app_type)
-    test_settings = get_test_settings(main_test_dir, app_type)
-
-    shutil.copyfile(test_settings[0], test_settings[1])
-
-    runtests_py_map = {
-        "wagtail": "./runtests.py",
-        "django_filter": "./runtests.py",
-        "django_rest_framework": "./runtests.py",
-        "default": os.path.join("src", "django", "tests", "runtests.py"),
-    }
+    test_settings = copy_test_settings(main_test_dir, app_type)
 
     command = [runtests_py_map[app_type]]
 
