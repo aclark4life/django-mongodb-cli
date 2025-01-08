72b3090ff504622e60aef29898706b13d00620a2
Jeffrey A. Clark <aclark@aclark.net>
2025-01-08 15:34:30 -0500

    Add wagtail option

--- django_mongodb_cli/makemigrations.py
+++ django_mongodb_cli/makemigrations.py
@@ -5,13 +5,17 @@ import subprocess
 
 
 @click.command(context_settings={"ignore_unknown_options": True})
+@click.option("-w", "--wagtail", is_flag=True, help="Run makemigrations for Wagtail.")
 @click.argument("args", nargs=-1)
-def makemigrations(args):
+def makemigrations(args, wagtail):
     """Run makemigrations."""
 
     if os.path.exists("manage.py"):
         command = [sys.executable, "manage.py", "makemigrations"]
     else:
         command = ["django-admin", "makemigrations"]  # Use a list for consistency
+
+    if wagtail:
+        os.environ["DJANGO_SETTINGS_MODULE"] = "wagtail.test.settings"
 
     subprocess.run(command + [*args])
