7554f8644f6c1c016ad12483315146c5bcd23864
Jeffrey A. Clark <aclark@aclark.net>
2025-01-07 21:11:40 -0500

    Refactor

--- django_mongodb_cli/runtests.py
+++ django_mongodb_cli/runtests.py
@@ -4,71 +4,66 @@ import shutil
 import subprocess
 
 
-def _copy_mongo_apps(
-    test_dir, wagtail=False, django_filter=False, django_rest_framework=False
-):
-    if wagtail:
-        click.echo(click.style("Copying mongo_apps to wagtail", fg="blue"))
+def copy_mongo_apps(test_dir, app_type):
+    """Copy the appropriate mongo_apps file based on the app type."""
+    app_files = {
+        "wagtail": "apps_wagtail.py",
+        "django_filter": "apps_filter.py",
+        "django_rest_framework": "apps_drf.py",
+    }
+    if app_type in app_files:
+        click.echo(click.style(f"Copying mongo_apps to {app_type}", fg="blue"))
         shutil.copyfile(
-            os.path.join("test_apps", "apps_wagtail.py"),
-            os.path.join(test_dir, "mongo_apps.py"),
-        )
-    elif django_filter:
-        click.echo(click.style("Copying mongo_apps to django-filter", fg="blue"))
-        shutil.copyfile(
-            os.path.join("test_apps", "apps_filter.py"),
-            os.path.join(test_dir, "mongo_apps.py"),
-        )
-    elif django_rest_framework:
-        click.echo(
-            click.style("Copying mongo_apps to django-rest-framework", fg="blue")
-        )
-        shutil.copyfile(
-            os.path.join("test_apps", "apps_drf.py"),
+            os.path.join("test_apps", app_files[app_type]),
             os.path.join(test_dir, "mongo_apps.py"),
         )
 
 
-def _copy_mongo_migrations(test_dir):
+def copy_mongo_migrations(test_dir):
+    """Copy mongo_migrations to the specified test directory."""
     click.echo(click.style("Copying mongo_migrations", fg="blue"))
-    if not os.path.exists(os.path.join(test_dir, "mongo_migrations")):
+    target_dir = os.path.join(test_dir, "mongo_migrations")
+    if not os.path.exists(target_dir):
         shutil.copytree(
             os.path.join("project_templates", "project_template", "mongo_migrations"),
-            os.path.join(test_dir, "mongo_migrations"),
+            target_dir,
         )
 
 
-def _get_test_settings(
-    test_dir, wagtail=False, django_filter=False, django_rest_framework=False
-):
-    if wagtail:
-        return [
-            os.path.join("test_settings", "settings_wagtail.py"),
-            os.path.join(test_dir, "mongo_settings.py"),
+def get_test_settings(test_dir, app_type):
+    """Retrieve settings for the specified app type."""
+    settings_map = {
+        "wagtail": [
+            "settings_wagtail.py",
+            "mongo_settings.py",
             "wagtail.test.mongo_settings",
             os.path.join("src", "wagtail"),
-        ]
-    elif django_filter:
-        return [
-            os.path.join("test_settings", "settings_filter.py"),
-            os.path.join(test_dir, "mongo_settings.py"),
+        ],
+        "django_filter": [
+            "settings_filter.py",
+            "mongo_settings.py",
             "tests.mongo_settings",
             os.path.join("src", "django-filter"),
-        ]
-    elif django_rest_framework:
-        return [
-            os.path.join("test_settings", "settings_drf.py"),
-            os.path.join(test_dir, "conftest.py"),
+        ],
+        "django_rest_framework": [
+            "settings_drf.py",
+            "conftest.py",
             "tests.conftest",
             os.path.join("src", "django-rest-framework"),
-        ]
-    else:
-        return [
-            os.path.join("test_settings", "settings_django.py"),
-            os.path.join(test_dir, "mongo_settings.py"),
+        ],
+        "default": [
+            "settings_django.py",
+            "mongo_settings.py",
             "mongo_settings",
             ".",
-        ]
+        ],
+    }
+    return [
+        os.path.join("test_settings", settings_map[app_type][0]),
+        os.path.join(test_dir, settings_map[app_type][1]),
+        settings_map[app_type][2],
+        settings_map[app_type][3],
+    ]
 
 
 @click.command()
@@ -91,55 +86,62 @@ def runtests(
     """
     click.echo(click.style("Running tests", fg="blue"))
 
-    test_dirs = []
-
-    if wagtail:
-        test_dirs.append(os.path.join("src", "wagtail", "wagtail", "test"))
-        test_dirs.append(os.path.join("src", "wagtail", "wagtail", "tests"))
-        runtests_py = "./runtests.py"
-        _copy_mongo_migrations(os.path.join("src", "wagtail", "wagtail", "test"))
-        _copy_mongo_apps(
-            os.path.join("src", "wagtail", "wagtail", "test"), wagtail=True
-        )
-        test_settings = _get_test_settings(test_dirs[0], wagtail=True)
-    elif django_filter:
-        test_dirs.append(os.path.join("src", "django-filter", "tests"))
-        _copy_mongo_migrations(os.path.join("src", "django-filter", "tests"))
-        _copy_mongo_apps(
-            os.path.join("src", "django-filter", "tests"), django_filter=True
-        )
-        runtests_py = "./runtests.py"
-        test_settings = _get_test_settings(test_dirs[0], django_filter=True)
-    elif django_rest_framework:
-        test_dirs.append(os.path.join("src", "django-rest-framework", "tests"))
-        _copy_mongo_migrations(os.path.join("src", "django-rest-framework", "tests"))
-        _copy_mongo_apps(
-            os.path.join("src", "django-rest-framework", "tests"),
-            django_rest_framework=True,
-        )
-        # runtests_py = os.path.join("src", "django-rest-framework", "runtests.py")
-        runtests_py = "./runtests.py"
-        test_settings = _get_test_settings(test_dirs[0], django_rest_framework=True)
-    else:
-        test_dirs.append(os.path.join("src", "django", "tests"))
-        test_dirs.append(os.path.join("src", "django-mongodb-backend", "tests"))
-        runtests_py = os.path.join(test_dirs[0], "runtests.py")
-        test_settings = _get_test_settings(test_dirs[0])
-
-    shutil.copyfile(
-        test_settings[0],
-        test_settings[1],
+    app_type = (
+        "wagtail"
+        if wagtail
+        else "django_filter"
+        if django_filter
+        else "django_rest_framework"
+        if django_rest_framework
+        else "default"
     )
 
-    command = [runtests_py]
+    test_dirs_map = {
+        "wagtail": [
+            os.path.join("src", "wagtail", "wagtail", "test"),
+            os.path.join("src", "wagtail", "wagtail", "tests"),
+        ],
+        "django_filter": [os.path.join("src", "django-filter", "tests")],
+        "django_rest_framework": [
+            os.path.join("src", "django-rest-framework", "tests")
+        ],
+        "default": [
+            os.path.join("src", "django", "tests"),
+            os.path.join("src", "django-mongodb-backend", "tests"),
+        ],
+    }
 
-    if not django_rest_framework:
-        command.extend(["--settings", test_settings[2]])
-        command.extend(["--parallel", "1"])
-        command.extend(["--verbosity", "3"])
-        command.extend(["--debug-sql"])
-        command.extend(["--noinput"])
+    test_dirs = test_dirs_map[app_type]
+    main_test_dir = test_dirs[0]
 
+    copy_mongo_migrations(main_test_dir)
+    copy_mongo_apps(main_test_dir, app_type)
+    test_settings = get_test_settings(main_test_dir, app_type)
+
+    shutil.copyfile(test_settings[0], test_settings[1])
+
+    runtests_py_map = {
+        "wagtail": "./runtests.py",
+        "django_filter": "./runtests.py",
+        "django_rest_framework": "./runtests.py",
+        "default": os.path.join("src", "django", "tests", "runtests.py"),
+    }
+
+    command = [runtests_py_map[app_type]]
+
+    if app_type != "django_rest_framework":
+        command.extend(
+            [
+                "--settings",
+                test_settings[2],
+                "--parallel",
+                "1",
+                "--verbosity",
+                "3",
+                "--debug-sql",
+                "--noinput",
+            ]
+        )
     command.extend(modules)
 
     if list_tests:
@@ -154,4 +156,4 @@ def runtests(
     if keyword:
         command.extend(["-k", keyword])
 
-    subprocess.run(command, stdin=None, stdout=None, stderr=None, cwd=cwd)
+    subprocess.run(command, cwd=cwd)
