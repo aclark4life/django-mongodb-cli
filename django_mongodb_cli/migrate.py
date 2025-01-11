0930ea71a471ec8beed58fb71e9fc36992719aa7
Jeffrey A. Clark <aclark@aclark.net>
2025-01-11 17:33:36 -0500

    Fix migrate

--- django_mongodb_cli/migrate.py
+++ django_mongodb_cli/migrate.py
@@ -3,43 +3,15 @@ import os
 import sys
 import subprocess
 
-from .utils import (
-    copy_test_settings,
-    test_dirs_map,
-    copy_mongo_apps,
-    copy_mongo_migrations,
-)
 
-
-@click.command()
-@click.option("-w", "--wagtail", is_flag=True, help="Run migrate for Wagtail.")
+@click.command(context_settings={"ignore_unknown_options": True})
 @click.argument("args", nargs=-1)
-def migrate(wagtail, args):
-    """Run Django migrations."""
+def manage(args):
+    """Run management commands."""
 
     if os.path.exists("manage.py"):
         command = [sys.executable, "manage.py", "migrate"]
     else:
         command = ["django-admin", "migrate"]  # Use a list for consistency
-
-    app_type = (
-        "wagtail"
-        if wagtail
-        # else "django_filter"
-        # if django_filter
-        # else "django_rest_framework"
-        # if django_rest_framework
-        else "default"
-    )
-
-    test_dirs = test_dirs_map[app_type]
-    test_dir = test_dirs[0]
-
-    copy_mongo_migrations(test_dir)
-    copy_mongo_apps(test_dir, app_type)
-    copy_test_settings(test_dir, app_type)
-
-    if app_type == "wagtail":
-        os.environ["DJANGO_SETTINGS_MODULE"] = "wagtail.test.mongo_settings"
 
     subprocess.run(command + [*args])
