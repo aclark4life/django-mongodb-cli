c9a6d7caba697a2ae482d7a9efd365e27693cae6
Jeffrey A. Clark <aclark@aclark.net>
2025-02-11 16:13:34 -0500

    Update install

--- django_mongodb_cli/install.py
+++ django_mongodb_cli/install.py
@@ -1,12 +1,39 @@
 import click
+import os
+import subprocess
+import sys
 
 
-from .utils import get_management_command
+from .utils import get_management_command, get_repos
 
 
 @click.command()
-def install():
+@click.option(
+    "-a",
+    "--all",
+    is_flag=True,
+    help="Install repos.",
+)
+def install(all):
     """Install development dependencies"""
+
+    repos, url_pattern, branch_pattern, upstream_pattern = get_repos("pyproject.toml")
 
     command = get_management_command()
     click.echo(f"Installing development dependencies using {' '.join(command)}...")
+
+    if all:
+        for repo_entry in repos:
+            url_match = url_pattern.search(repo_entry)
+            if url_match:
+                repo_url = url_match.group(0)
+                repo_name = os.path.basename(repo_url)
+                clone_path = os.path.join("src", repo_name)
+                if os.path.exists("pyproject.toml"):
+                    subprocess.run(
+                        [sys.executable, "-m", "pip", "install", "-e", clone_path]
+                    )
+                if os.path.exists("setup.py"):
+                    subprocess.run(
+                        [sys.executable, "setup.py", "develop"], cwd=clone_path
+                    )
