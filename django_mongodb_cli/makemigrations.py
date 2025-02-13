7187aea532d0d95f67255edd45e61c1b052b788c
Jeffrey A. Clark <aclark@aclark.net>
2025-02-12 19:22:03 -0500

    Clean up

--- django_mongodb_cli/makemigrations.py
+++ django_mongodb_cli/makemigrations.py
@@ -1,11 +1,9 @@
 import click
-import os
 import subprocess
 
 from .utils import (
     copy_mongo_apps,
     copy_test_settings,
-    delete_mongo_migrations,
     get_app_type,
     get_management_command,
     test_dirs_map,
@@ -47,9 +45,6 @@ def makemigrations(
     django_debug_toolbar,
 ):
     """Run makemigrations."""
-
-    command = get_management_command("makemigrations")
-
     app_type = get_app_type(
         django_allauth,
         django_debug_toolbar,
@@ -57,19 +52,9 @@ def makemigrations(
         django_rest_framework,
         wagtail,
     )
-
     test_dirs = test_dirs_map[app_type]
     test_dir = test_dirs[0]
-
-    if delete:
-        if app_type == "wagtail":
-            delete_mongo_migrations(
-                os.path.join(test_dir, "mongo_migrations"),
-                os.path.join("src", "wagtail"),
-            )
-            click.echo("Deleted migrations directory.")
-            return
-
     copy_mongo_apps(test_dir, app_type)
     copy_test_settings(test_dir, app_type)
+    command = get_management_command("makemigrations")
     subprocess.run(command + [*args])
