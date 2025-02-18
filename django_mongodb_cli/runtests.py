3b33d1d65a640a3020576c216f2dbed6d7b8b7c8
Jeffrey A. Clark <aclark@aclark.net>
2025-02-18 12:51:41 -0500

    Replace runtests with repo example

--- django_mongodb_cli/runtests.py
+++ django_mongodb_cli/runtests.py
@@ -1,94 +1,291 @@
-import click
 import os
+import sys
 import subprocess
 
-
-from .config import runtests_map, test_dirs_map
-
-
-from .utils import (
-    apply_patches,
-    copy_mongo_migrations,
-    copy_mongo_apps,
-    copy_test_settings,
-    get_app_type,
-)
+import click
+from .utils import get_repos, clone_from, add_remote, pull
 
 
-@click.command()
-@click.argument("modules", nargs=-1)
-@click.option("-a", "--django-allauth", help="Run Django Allauth tests", is_flag=True)
+class Repo:
+    def __init__(self, home):
+        self.home = home
+        self.config = {}
+        self.verbose = False
+
+    def set_config(self, key, value):
+        self.config[key] = value
+        if self.verbose:
+            click.echo(f"  config[{key}] = {value}", file=sys.stderr)
+
+    def __repr__(self):
+        return f"<Repo {self.home}>"
+
+
+pass_repo = click.make_pass_decorator(Repo)
+
+
+@click.group()
 @click.option(
-    "-d", "--django-debug-toolbar", help="Run Django Debug Toolbar tests", is_flag=True
+    "--repo-home",
+    envvar="REPO_HOME",
+    default=".repo",
+    metavar="PATH",
+    help="Changes the repository folder location.",
 )
-@click.option("-k", "--keyword", help="Filter tests by keyword")
-@click.option("-f", "--django-filter", help="Run Django Filter tests", is_flag=True)
 @click.option(
-    "-r",
-    "--django-rest-framework",
-    help="Run Django Rest Framework tests",
+    "--config",
+    nargs=2,
+    multiple=True,
+    metavar="KEY VALUE",
+    help="Overrides a config key/value pair.",
+)
+@click.option("--verbose", "-v", is_flag=True, help="Enables verbose mode.")
+@click.version_option("1.0")
+@click.pass_context
+def repo(ctx, repo_home, config, verbose):
+    """Repo is a command line tool that showcases how to build complex
+    command line interfaces with Click.
+
+    This tool is supposed to look like a distributed version control
+    system to show how something like this can be structured.
+    """
+    # Create a repo object and remember it as as the context object.  From
+    # this point onwards other commands can refer to it by using the
+    # @pass_repo decorator.
+    ctx.obj = Repo(os.path.abspath(repo_home))
+    ctx.obj.verbose = verbose
+    for key, value in config:
+        ctx.obj.set_config(key, value)
+
+
+@repo.command()
+@click.argument("src", required=False)
+@click.argument("dest", required=False)
+@click.option(
+    "-a",
+    "--all",
     is_flag=True,
+    help="Check out all branches/tracked files.",
 )
-@click.option("-l", "--list-tests", help="List tests", is_flag=True)
-@click.option("-w", "--wagtail", help="Run Wagtail tests", is_flag=True)
-def runtests(
-    modules,
-    keyword,
-    list_tests,
-    wagtail,
-    django_filter,
-    django_rest_framework,
-    django_allauth,
-    django_debug_toolbar,
-):
+@click.option(
+    "-l",
+    "--list",
+    is_flag=True,
+    help="List all branches/tracked files.",
+)
+@pass_repo
+def clone(repo, src, dest, all, list):
+    """Clones a repository.
+
+    This will clone the repository at SRC into the folder DEST.  If DEST
+    is not provided this will automatically use the last path component
+    of SRC and create that folder.
     """
-    Run `runtests.py` for Django or Wagtail.
-    """
-    click.echo(click.style("Running tests", fg="blue"))
-    app_type = get_app_type(
-        django_allauth,
-        django_debug_toolbar,
-        django_filter,
-        django_rest_framework,
-        wagtail,
-    )
-    test_dirs = test_dirs_map[app_type]
-    test_dir = test_dirs[0]
-    test_settings = copy_test_settings(test_dir, app_type)
-    command = [runtests_map[app_type]]
-    if list_tests:
-        for test_dir in test_dirs:
-            for module in sorted(os.listdir(test_dir)):
-                click.echo(module)
+    if dest is None:
+        dest = "src"
+    repo.home = dest
+    repos, url_pattern, branch_pattern, upstream_pattern = get_repos("pyproject.toml")
+
+    if list:
+        for repo_entry in repos:
+            click.echo(repo_entry)
         return
-    apply_patches(app_type)
-    copy_mongo_migrations(test_dir)
-    copy_mongo_apps(test_dir, app_type)
-    if (
-        app_type != "django_rest_framework"
-        and app_type != "django_allauth"
-        and app_type != "django_debug_toolbar"
-    ):
-        command.extend(
-            [
-                "--settings",
-                test_settings["module"],
-                "--parallel",
-                "1",
-                "--verbosity",
-                "3",
-                "--debug-sql",
-                "--noinput",
-            ]
+    if src:
+        for repo_entry in repos:
+            url_match = url_pattern.search(repo_entry)
+            branch_match = branch_pattern.search(repo_entry)
+            if url_match:
+                repo_url = url_match.group(0)
+                repo_name = os.path.basename(repo_url)
+                if repo_name == src:
+                    branch = branch_match.group(1) if branch_match else "main"
+                    clone_path = os.path.join(dest, repo_name)
+                    clone_from(repo_url, clone_path, branch)
+    if all:
+        click.echo(f"Checking out {len(repos)} repositories")
+        for repo_entry in repos:
+            url_match = url_pattern.search(repo_entry)
+            branch_match = branch_pattern.search(repo_entry)
+            if url_match:
+                repo_url = url_match.group(0)
+                repo_name = os.path.basename(repo_url)
+                branch = branch_match.group(1) if branch_match else "main"
+                clone_path = os.path.join(dest, repo_name)
+                clone_from(repo_url, clone_path, branch)
+            else:
+                click.echo(f"Invalid repository entry: {repo_entry}")
+
+
+@repo.command()
+@click.confirmation_option()
+@pass_repo
+def delete(repo):
+    """Deletes a repository.
+
+    This will throw away the current repository.
+    """
+    click.echo(f"Destroying repo {repo.home}")
+    click.echo("Deleted!")
+
+
+@repo.command()
+@click.option("--username", prompt=True, help="The developer's shown username.")
+@click.option("--email", prompt="E-Mail", help="The developer's email address")
+@click.password_option(help="The login password.")
+@pass_repo
+def setuser(repo, username, email, password):
+    """Sets the user credentials.
+
+    This will override the current user config.
+    """
+    repo.set_config("username", username)
+    repo.set_config("email", email)
+    repo.set_config("password", "*" * len(password))
+    click.echo("Changed credentials.")
+
+
+@repo.command()
+@click.option(
+    "--message",
+    "-m",
+    multiple=True,
+    help="The commit message.  If provided multiple times each"
+    " argument gets converted into a new line.",
+)
+@click.argument("files", nargs=-1, type=click.Path())
+@pass_repo
+def commit(repo, files, message):
+    """Commits outstanding changes.
+
+    Commit changes to the given files into the repository.  You will need to
+    "repo push" to push up your changes to other repositories.
+
+    If a list of files is omitted, all changes reported by "repo status"
+    will be committed.
+    """
+    if not message:
+        marker = "# Files to be committed:"
+        hint = ["", "", marker, "#"]
+        for file in files:
+            hint.append(f"#   U {file}")
+        message = click.edit("\n".join(hint))
+        if message is None:
+            click.echo("Aborted!")
+            return
+        msg = message.split(marker)[0].rstrip()
+        if not msg:
+            click.echo("Aborted! Empty commit message")
+            return
+    else:
+        msg = "\n".join(message)
+    click.echo(f"Files to be committed: {files}")
+    click.echo(f"Commit message:\n{msg}")
+
+
+@repo.command(short_help="Copies files.")
+@click.option(
+    "--force", is_flag=True, help="forcibly copy over an existing managed file"
+)
+@click.argument("src", nargs=-1, type=click.Path())
+@click.argument("dst", type=click.Path())
+@pass_repo
+def copy(repo, src, dst, force):
+    """Copies one or multiple files to a new location.  This copies all
+    files from SRC to DST.
+    """
+    for fn in src:
+        click.echo(f"Copy from {fn} -> {dst}")
+
+
+@repo.command()
+@click.option(
+    "-a",
+    "--all",
+    is_flag=True,
+    help="Install repos.",
+)
+@click.argument("src", nargs=-1, type=click.Path())
+@pass_repo
+def install(repo, src, all):
+    """Install development dependencies"""
+
+    if src:
+        clone_path = os.path.join("src", src[0])
+        subprocess.run([sys.executable, "-m", "pip", "install", "-e", clone_path])
+
+    if all:
+        repos, url_pattern, branch_pattern, upstream_pattern = get_repos(
+            "pyproject.toml"
         )
-    command.extend(modules)
-    if keyword:
-        command.extend(["-k", keyword])
-    click.echo(f"Running {' '.join(command)}")
-    if app_type == "django_debug_toolbar":
-        # Set the DJANGO_SETTINGS_MODULE environment variable
-        # for pytest to use the correct settings file.
-        os.environ["DJANGO_SETTINGS_MODULE"] = test_settings[2]
-    cwd = test_settings["path"]
-    click.echo(f"Working directory: {cwd}")
-    subprocess.run(command, cwd=cwd)
+        for repo_entry in repos:
+            url_match = url_pattern.search(repo_entry)
+            if url_match:
+                repo_url = url_match.group(0)
+                repo_name = os.path.basename(repo_url)
+                clone_path = os.path.join("src", repo_name)
+                if os.path.exists("pyproject.toml"):
+                    subprocess.run(
+                        [sys.executable, "-m", "pip", "install", "-e", clone_path]
+                    )
+                if os.path.exists("setup.py"):
+                    subprocess.run(
+                        [sys.executable, "setup.py", "develop"], cwd=clone_path
+                    )
+
+
+@repo.command()
+@click.argument("src", required=False)
+@click.argument("dest", required=False)
+@click.option(
+    "-a",
+    "--all",
+    is_flag=True,
+    help="Check out all branches/tracked files.",
+)
+@pass_repo
+def fetch(repo, src, dest, all):
+    """Upstream"""
+    if dest is None:
+        dest = "src"
+    repo.home = dest
+    repos, url_pattern, branch_pattern, upstream_pattern = get_repos("pyproject.toml")
+    if src:
+        click.echo(f"Fetching {src}")
+        for repo_entry in repos:
+            url_match = url_pattern.search(repo_entry)
+            if url_match:
+                repo_url = url_match.group(0)
+                repo_name = os.path.basename(repo_url)
+                if repo_name == src:
+                    clone_path = os.path.join(dest, repo_name)
+                    upstream_match = upstream_pattern.search(repo_entry)
+                    if upstream_match:
+                        add_remote(upstream_match, clone_path, repo_name)
+
+
+@repo.command()
+@click.argument("src", required=False)
+@click.argument("dest", required=False)
+@click.option(
+    "-a",
+    "--all",
+    is_flag=True,
+    help="Check out all branches/tracked files.",
+)
+@pass_repo
+def update(repo, src, dest, all):
+    """Update"""
+    if dest is None:
+        dest = "src"
+    repo.home = dest
+    repos, url_pattern, branch_pattern, upstream_pattern = get_repos("pyproject.toml")
+    if src:
+        click.echo(f"Updating {src}")
+        for repo_entry in repos:
+            url_match = url_pattern.search(repo_entry)
+            if url_match:
+                repo_url = url_match.group(0)
+                repo_name = os.path.basename(repo_url)
+                if repo_name == src:
+                    clone_path = os.path.join(dest, repo_name)
+                    pull(clone_path)
