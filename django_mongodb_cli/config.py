c06d5179664971736fc86c479c04adf51605dddf
Jeffrey A. Clark <aclark@aclark.net>
2025-02-18 21:18:57 -0500

    Rename

--- django_mongodb_cli/config.py
+++ django_mongodb_cli/config.py
@@ -2,7 +2,7 @@ import os
 
 test_settings_map = {
     "django": {
-        "src": os.path.join("settings", "settings_django.py"),
+        "src": os.path.join("settings_test", "settings_django.py"),
         "dest": os.path.join("src", "django", "tests", "mongo_settings.py"),
         "project_dir": os.path.join("src", "django"),
         "command": "./runtests.py",
@@ -10,7 +10,7 @@ test_settings_map = {
         "module": "mongo_settings",
     },
     "django_filter": {
-        "src": os.path.join("settings", "settings_filter.py"),
+        "src": os.path.join("settings_test", "settings_filter.py"),
         "dest": os.path.join(
             "src", "django-filter", "django_filters", "mongo_settings.py"
         ),
@@ -20,7 +20,7 @@ test_settings_map = {
         "module": "tests.mongo_settings",
     },
     "django_allauth": {
-        "src": os.path.join("settings", "settings_allauth_regular.py"),
+        "src": os.path.join("settings_test", "settings_allauth_regular.py"),
         "dest": "mongo_settings.py",
         "project_dir": os.path.join("src", "django-allauth"),
         "command": "tox",
@@ -28,7 +28,7 @@ test_settings_map = {
         "module": "mongo_settings",
     },
     "django_debug_toolbar": {
-        "src": os.path.join("settings", "settings_debug_toolbar.py"),
+        "src": os.path.join("settings_test", "settings_debug_toolbar.py"),
         "dest": "mongo_settings.py",
         "project_dir": os.path.join("src", "django-debug-toolbar"),
         "command": "pytest",
@@ -36,7 +36,7 @@ test_settings_map = {
         "module": "mongo_settings",
     },
     "django_rest_framework": {
-        "src": os.path.join("settings", "settings_drf.py"),
+        "src": os.path.join("settings_test", "settings_drf.py"),
         "dest": "conftest.py",
         "project_dir": os.path.join("src", "django-rest-framework"),
         "command": "./runtests.py",
@@ -44,7 +44,7 @@ test_settings_map = {
         "module": "mongo_settings",
     },
     "wagtail": {
-        "src": os.path.join("settings", "settings_wagtail.py"),
+        "src": os.path.join("settings_test", "settings_wagtail.py"),
         "dest": "mongo_settings.py",
         "project_dir": os.path.join("src", "wagtail"),
         "command": "./runtests.py",
