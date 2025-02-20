2194007cb85a4424473eed4a7e4b3ac64149232b
Jeffrey A. Clark <aclark@aclark.net>
2025-02-19 23:04:48 -0500

    Clean up

--- settings_test/settings_drf.py
+++ settings_test/settings_drf.py
@@ -17,13 +17,6 @@ def pytest_addoption(parser):
     )
 
 
-MIGRATION_MODULES = {
-    "admin": "tests.mongo_migrations.admin",
-    "auth": "tests.mongo_migrations.auth",
-    "contenttypes": "tests.mongo_migrations.contenttypes",
-}
-
-
 def pytest_configure(config):
     from django.conf import settings
 
