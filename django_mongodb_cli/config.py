437047b7f2fb4fc28cd32a5d2c89cd4a18ff2eea
Jeffrey A. Clark <aclark@aclark.net>
2025-02-20 16:03:10 -0500

    Fix wagtail

--- django_mongodb_cli/config.py
+++ django_mongodb_cli/config.py
@@ -100,6 +100,32 @@ test_settings_map = {
         },
         "test_dirs": [os.path.join("src", "django-rest-framework", "tests")],
     },
+    "wagtail": {
+        "apps": {
+            "src": os.path.join("stage", "apps", "apps_wagtail.py"),
+            "target": os.path.join("src", "wagtail", "tests", "mongo_apps.py"),
+        },
+        "migrations_dir": {
+            "src": os.path.join("src", "django-mongodb-project", "mongo_migrations"),
+            "target": os.path.join("src", "wagtail", "tests", "mongo_migrations"),
+        },
+        "command": "./runtests.py",
+        "cwd": os.path.join("src", "wagtail"),
+        "project_dir": os.path.join("src", "django-rest-framework"),
+        "settings_file": {
+            "test": {
+                "src": os.path.join("stage", "settings", "settings_drf.py"),
+                "target": os.path.join(
+                    "src", "wagtail", "wagtail", "tests", "mongo_settings.py"
+                ),
+            },
+        },
+        "settings_module": {
+            "test": "wagtail.tests.mongo_settings",
+            "migrate": "wagtail.tests.mongo_settings",
+        },
+        "test_dirs": [os.path.join("src", "wagtail", "wagtail", "tests")],
+    },
     "django_allauth": {
         "command": "tox",
         "project_dir": os.path.join("src", "django-allauth"),
@@ -113,14 +139,6 @@ test_settings_map = {
         "project_dir": os.path.join("src", "django-debug-toolbar"),
         "settings_module": "mongo_settings",
         "src": os.path.join("stage", "settings", "settings_debug_toolbar.py"),
-        "target": "mongo_settings.py",
-        "test_dirs": ["tests"],
-    },
-    "wagtail": {
-        "command": "./runtests.py",
-        "project_dir": os.path.join("src", "wagtail"),
-        "settings_module": "mongo_settings",
-        "src": os.path.join("stage", "settings", "settings_wagtail.py"),
         "target": "mongo_settings.py",
         "test_dirs": ["tests"],
     },
