2b1c4c6b0fa2026c36791a65815a5dbd9d036f6b
Jeffrey A. Clark <aclark@aclark.net>
2025-02-26 18:17:56 -0500

    Move runtests to repo

--- django_mongodb_cli/__init__.py
+++ django_mongodb_cli/__init__.py
@@ -6,7 +6,6 @@ from .manage import manage
 from .migrate import migrate
 from .repo import repo
 from .runserver import runserver
-from .runtests import runtests
 from .shell import shell
 from .startapp import startapp
 from .startproject import startproject
@@ -24,6 +23,5 @@ cli.add_command(manage)
 cli.add_command(migrate)
 cli.add_command(repo)
 cli.add_command(runserver)
-cli.add_command(runtests)
 cli.add_command(startapp)
 cli.add_command(startproject)
