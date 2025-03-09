cc6716a52ea8217e8c3442b6dafb17031dda4b2a
Jeffrey A. Clark <aclark@aclark.net>
2025-03-09 14:36:38 -0400

    Refactor

--- django_mongodb_cli/config.py
+++ django_mongodb_cli/config.py
@@ -1,139 +1,127 @@
-import os
+from os.path import join
 
 test_settings_map = {
     "django": {
-        "cwd": os.path.join("src", "django", "tests"),
+        "cwd": join("src", "django", "tests"),
         "command": "./runtests.py",
         "migrations_dir": {
-            "src": os.path.join(
+            "src": join(
                 "src",
                 "django-mongodb-templates",
                 "project_template",
                 "mongo_migrations",
             ),
-            "target": os.path.join("src", "django", "tests"),
+            "target": join("src", "django", "tests"),
         },
-        "project_dir": os.path.join("src", "django"),
+        "project_dir": join("src", "django"),
         "settings_file": {
             "test": {
-                "src": os.path.join("settings", "django_settings.py"),
-                "target": os.path.join("src", "django", "tests", "mongo_settings.py"),
+                "src": join("settings", "django_settings.py"),
+                "target": join("src", "django", "tests", "mongo_settings.py"),
             },
             "migrate": {
-                "src": os.path.join("settings", "django_settings.py"),
-                "target": os.path.join("src", "django", "tests", "mongo_settings.py"),
+                "src": join("settings", "django_settings.py"),
+                "target": join("src", "django", "tests", "mongo_settings.py"),
             },
         },
         "settings_module": {
             "test": "mongo_settings",
             "migrate": "mongo_settings",
         },
-        "target": os.path.join("src", "django", "tests", "mongo_settings.py"),
+        "target": join("src", "django", "tests", "mongo_settings.py"),
         "test_dirs": [
-            os.path.join("src", "django", "tests"),
-            os.path.join("src", "django-mongodb-backend", "tests"),
+            join("src", "django", "tests"),
+            join("src", "django-mongodb-backend", "tests"),
         ],
     },
     "django-filter": {
         "apps": {
-            "src": os.path.join("settings", "filter_apps.py"),
-            "target": os.path.join("src", "django-filter", "tests", "mongo_apps.py"),
+            "src": join("settings", "filter_apps.py"),
+            "target": join("src", "django-filter", "tests", "mongo_apps.py"),
         },
         "command": "./runtests.py",
-        "cwd": os.path.join("src", "django-filter"),
+        "cwd": join("src", "django-filter"),
         "migrations_dir": {
-            "src": os.path.join(
+            "src": join(
                 "src",
                 "django-mongodb-templates",
                 "project_template",
                 "mongo_migrations",
             ),
-            "target": os.path.join("src", "django-filter", "tests", "mongo_migrations"),
+            "target": join("src", "django-filter", "tests", "mongo_migrations"),
         },
-        "project_dir": os.path.join("src", "django-filter"),
+        "project_dir": join("src", "django-filter"),
         "settings_file": {
             "test": {
-                "src": os.path.join("settings", "filter_settings.py"),
-                "target": os.path.join("src", "django-filter", "tests", "settings.py"),
+                "src": join("settings", "filter_settings.py"),
+                "target": join("src", "django-filter", "tests", "settings.py"),
             },
             "migrate": {
-                "src": os.path.join("settings", "filter_settings.py"),
-                "target": os.path.join("src", "django-filter", "tests", "settings.py"),
+                "src": join("settings", "filter_settings.py"),
+                "target": join("src", "django-filter", "tests", "settings.py"),
             },
         },
         "settings_module": {
             "test": "tests.settings",
             "migrate": "tests.settings",
         },
-        "test_dirs": [os.path.join("src", "django-filter", "tests")],
+        "test_dirs": [join("src", "django-filter", "tests")],
     },
     "django-rest-framework": {
         "apps": {
-            "src": os.path.join("settings", "rest_framework_apps.py"),
-            "target": os.path.join(
-                "src", "django-rest-framework", "tests", "mongo_apps.py"
-            ),
+            "src": join("settings", "rest_framework_apps.py"),
+            "target": join("src", "django-rest-framework", "tests", "mongo_apps.py"),
         },
         "migrations_dir": {
-            "src": os.path.join("src", "django-mongodb-project", "mongo_migrations"),
-            "target": os.path.join(
-                "src", "django-rest-framework", "tests", "mongo_migrations"
-            ),
+            "src": join("src", "django-mongodb-project", "mongo_migrations"),
+            "target": join("src", "django-rest-framework", "tests", "mongo_migrations"),
         },
         "command": "./runtests.py",
-        "cwd": os.path.join("src", "django-rest-framework"),
-        "project_dir": os.path.join("src", "django-rest-framework"),
+        "cwd": join("src", "django-rest-framework"),
+        "project_dir": join("src", "django-rest-framework"),
         "settings_file": {
             "test": {
-                "src": os.path.join("settings", "rest_framework_settings.py"),
-                "target": os.path.join(
-                    "src", "django-rest-framework", "tests", "conftest.py"
-                ),
+                "src": join("settings", "rest_framework_settings.py"),
+                "target": join("src", "django-rest-framework", "tests", "conftest.py"),
             },
             "migrate": {
-                "src": os.path.join("settings", "rest_framework_migrate.py"),
-                "target": os.path.join(
-                    "src", "django-rest-framework", "tests", "conftest.py"
-                ),
+                "src": join("settings", "rest_framework_migrate.py"),
+                "target": join("src", "django-rest-framework", "tests", "conftest.py"),
             },
         },
         "settings_module": {
             "test": "tests.conftest",
             "migrate": "tests.conftest",
         },
-        "test_dirs": [os.path.join("src", "django-rest-framework", "tests")],
+        "test_dirs": [join("src", "django-rest-framework", "tests")],
     },
     "wagtail": {
         "apps": {
-            "src": os.path.join("settings", "apps_wagtail.py"),
-            "target": os.path.join(
-                "src", "wagtail", "wagtail", "test", "mongo_apps.py"
-            ),
+            "src": join("settings", "apps_wagtail.py"),
+            "target": join("src", "wagtail", "wagtail", "test", "mongo_apps.py"),
         },
         "migrations_dir": {
-            "src": os.path.join(
+            "src": join(
                 "src",
                 "django-mongodb-templates",
                 "project_template",
                 "mongo_migrations",
             ),
-            "target": os.path.join(
-                "src", "wagtail", "wagtail", "test", "mongo_migrations"
-            ),
+            "target": join("src", "wagtail", "wagtail", "test", "mongo_migrations"),
         },
         "command": "./runtests.py",
-        "cwd": os.path.join("src", "wagtail"),
-        "project_dir": os.path.join("src", "wagtail"),
+        "cwd": join("src", "wagtail"),
+        "project_dir": join("src", "wagtail"),
         "settings_file": {
             "test": {
-                "src": os.path.join("settings", "settings_wagtail.py"),
-                "target": os.path.join(
+                "src": join("settings", "settings_wagtail.py"),
+                "target": join(
                     "src", "wagtail", "wagtail", "test", "mongo_settings.py"
                 ),
             },
             "migrate": {
-                "src": os.path.join("settings", "settings_wagtail.py"),
-                "target": os.path.join(
+                "src": join("settings", "settings_wagtail.py"),
+                "target": join(
                     "src", "wagtail", "wagtail", "test", "mongo_settings.py"
                 ),
             },
@@ -143,30 +131,30 @@ test_settings_map = {
             "migrate": "wagtail.test.mongo_settings",
         },
         "test_dirs": [
-            os.path.join("src", "wagtail", "wagtail", "tests"),
-            os.path.join("src", "wagtail", "wagtail", "test"),
+            join("src", "wagtail", "wagtail", "tests"),
+            join("src", "wagtail", "wagtail", "test"),
         ],
     },
     "django-debug-toolbar": {
         "apps": {
-            "src": os.path.join("settings", "debug_toolbar_apps.py"),
-            "target": os.path.join(
+            "src": join("settings", "debug_toolbar_apps.py"),
+            "target": join(
                 "src", "django-debug-toolbar", "debug_toolbar", "mongo_apps.py"
             ),
         },
         "command": "python",
-        "cwd": os.path.join("src", "django-debug-toolbar"),
-        "project_dir": os.path.join("src", "django-debug-toolbar"),
+        "cwd": join("src", "django-debug-toolbar"),
+        "project_dir": join("src", "django-debug-toolbar"),
         "settings_file": {
             "test": {
-                "src": os.path.join("settings", "debug_toolbar_settings.py"),
-                "target": os.path.join(
+                "src": join("settings", "debug_toolbar_settings.py"),
+                "target": join(
                     "src", "django-debug-toolbar", "debug_toolbar", "mongo_settings.py"
                 ),
             },
             "migrate": {
-                "src": os.path.join("settings", "debug_toolbar_settings.py"),
-                "target": os.path.join(
+                "src": join("settings", "debug_toolbar_settings.py"),
+                "target": join(
                     "src", "django-debug-toolbar", "debug_toolbar", "mongo_settings.py"
                 ),
             },
@@ -179,24 +167,20 @@ test_settings_map = {
     },
     "django-allauth": {
         "apps": {
-            "src": os.path.join("settings", "allauth_apps.py"),
-            "target": os.path.join("src", "django-allauth", "allauth", "mongo_apps.py"),
+            "src": join("settings", "allauth_apps.py"),
+            "target": join("src", "django-allauth", "allauth", "mongo_apps.py"),
         },
         "command": "python",
-        "cwd": os.path.join("src", "django-allauth"),
-        "project_dir": os.path.join("src", "django-allauth"),
+        "cwd": join("src", "django-allauth"),
+        "project_dir": join("src", "django-allauth"),
         "settings_file": {
             "test": {
-                "src": os.path.join("settings", "allauth_settings.py"),
-                "target": os.path.join(
-                    "src", "django-allauth", "allauth", "mongo_settings.py"
-                ),
+                "src": join("settings", "allauth_settings.py"),
+                "target": join("src", "django-allauth", "allauth", "mongo_settings.py"),
             },
             "migrate": {
-                "src": os.path.join("settings", "allauth_settings.py"),
-                "target": os.path.join(
-                    "src", "django-allauth", "allauth", "mongo_settings.py"
-                ),
+                "src": join("settings", "allauth_settings.py"),
+                "target": join("src", "django-allauth", "allauth", "mongo_settings.py"),
             },
         },
         "settings_module": {
@@ -204,11 +188,9 @@ test_settings_map = {
             "migrate": "allauth.mongo_settings",
         },
         "migrations_dir": {
-            "src": os.path.join("src", "django-mongodb-project", "mongo_migrations"),
-            "target": os.path.join(
-                "src", "django-allauth", "allauth", "mongo_migrations"
-            ),
+            "src": join("src", "django-mongodb-project", "mongo_migrations"),
+            "target": join("src", "django-allauth", "allauth", "mongo_migrations"),
         },
-        "test_dirs": [os.path.join("src", "django-allauth", "tests")],
+        "test_dirs": [join("src", "django-allauth", "tests")],
     },
 }
