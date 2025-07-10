bb355f101289afea32dd88bdf5284255bab38473
Jeffrey A. Clark <aclark@aclark.net>
2025-07-10 17:24:39 -0400

    Update settings

--- test/settings/django.py
+++ test/settings/django.py
@@ -5,14 +5,10 @@ from django_mongodb_backend import encryption, parse_uri
 # Queryable Encryption settings
 KEY_VAULT_NAMESPACE = encryption.get_key_vault_namespace()
 KMS_PROVIDERS = encryption.get_kms_providers()
-KMS_PROVIDER = encryption.KMS_PROVIDER
 AUTO_ENCRYPTION_OPTS = encryption.get_auto_encryption_opts(
     key_vault_namespace=KEY_VAULT_NAMESPACE,
     kms_providers=KMS_PROVIDERS,
 )
-
-ENCRYPTED_DB_ALIAS = encryption.ENCRYPTED_DB_ALIAS
-ENCRYPTED_APPS = encryption.ENCRYPTED_APPS
 
 DATABASE_URL = os.environ.get("MONGODB_URI", "mongodb://localhost:27017")
 DATABASES = {
@@ -20,10 +16,10 @@ DATABASES = {
         DATABASE_URL,
         db_name="test",
     ),
-    ENCRYPTED_DB_ALIAS: parse_uri(
+    "encrypted": parse_uri(
         DATABASE_URL,
         options={"auto_encryption_opts": AUTO_ENCRYPTION_OPTS},
-        db_name=ENCRYPTED_DB_ALIAS,
+        db_name="encrypted",
     ),
 }
 
