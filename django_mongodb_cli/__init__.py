d8b7c3e7565ec7a23ef4ab48d7f4276fc0a22ed9
Jeffrey A. Clark <aclark@aclark.net>
2025-07-26 22:24:53 -0400

    Add -h

--- django_mongodb_cli/__init__.py
+++ django_mongodb_cli/__init__.py
@@ -1,15 +1,13 @@
 import sys
 import typer
 
-from .app import app
-from .project import project
 from .repo import repo
 
 dm = typer.Typer(
-    help="\n\nDjango MongoDB CLI\n\nSystem executable:\n\n" + sys.executable
+    help="\n\nDjango MongoDB CLI\n\nSystem executable:\n\n" + sys.executable,
+    add_completion=False,
+    context_settings={"help_option_names": ["-h", "--help"]},
 )
 
 
-dm.add_typer(app, name="app")
-dm.add_typer(project, name="project")
 dm.add_typer(repo, name="repo")
